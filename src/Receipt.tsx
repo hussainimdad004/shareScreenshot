8b3e2be0a20bc488e7897b3c2a5d3b909bb8678c
Imdad Hussain <hussain.imdad004@gmail.com>
2024-03-28 08:32:25 +0200

    added receipt styles. also icons and user picture

--- src/Receipt.tsx
+++ src/Receipt.tsx
@@ -1,21 +1,39 @@
-// Receipt.tsx
-
 import React, {useRef} from 'react';
-import {View, Button, StyleSheet, Text} from 'react-native';
-import ViewShot from 'react-native-view-shot'; // Import ViewShot component
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import ViewShot from 'react-native-view-shot';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
-const Receipt = ({
-  onTakeScreenshot,
-}: {
+interface ReceiptProps {
   onTakeScreenshot: (uri: string) => void;
-}) => {
-  const viewRef = useRef<ViewShot | null>(null); // Reference to ViewShot component
+}
+
+interface DataItemProps {
+  firstEntry: string;
+  secondEntry: string;
+}
+
+const DataItem: React.FC<DataItemProps> = ({firstEntry, secondEntry}) => (
+  <View style={styles.dataItemStyle}>
+    <Text style={styles.firstEntryText}>{firstEntry}</Text>
+    <Text style={styles.secondEntryText}>{secondEntry}</Text>
+  </View>
+);
+
+const Receipt: React.FC<ReceiptProps> = ({onTakeScreenshot}) => {
+  const viewRef = useRef<ViewShot>(null);
 
   const handleTakeScreenshot = async () => {
     try {
       if (viewRef.current && viewRef.current.capture) {
-        const uri = await viewRef.current.capture(); // Capture screenshot
-        onTakeScreenshot(uri); // Pass captured image URI to parent component
+        const uri = await viewRef.current.capture();
+        onTakeScreenshot(uri);
       }
     } catch (error) {
       console.error('Failed to capture screenshot: ', error);
@@ -24,60 +42,159 @@ const Receipt = ({
 
   return (
     <View style={styles.receiptContainer}>
+      <View style={styles.headerContainer}>
+        <FontAwesomeIcon name="check-square-o" size={40} color="#fff" />
+        <Text style={styles.headerTitle}>Successful Transaction</Text>
+      </View>
       <ViewShot ref={viewRef} style={styles.container}>
-        <Text style={styles.header}>Receipt</Text>
-        <View style={styles.row}>
-          <Text style={styles.label}>Date:</Text>
-          <Text style={styles.content}>March 26, 2024</Text>
+        <Text style={styles.transferToText}>Transfer to</Text>
+        <View style={styles.transferToContainer}>
+          <View style={styles.userDataContainer}>
+            <Image
+              source={{
+                uri: 'https://source.unsplash.com/random/150x150/?pakistan',
+              }}
+              style={styles.image}
+            />
+            <View style={styles.nameAndPhoneContainer}>
+              <Text style={styles.nameText}>Ryan Green</Text>
+              <Text style={styles.phoneText}>(618) 878-1478</Text>
+            </View>
+          </View>
+          <FontAwesomeIcon name="star-o" size={20} color="#91959f" />
         </View>
-        <View style={styles.row}>
-          <Text style={styles.label}>Merchant:</Text>
-          <Text style={styles.content}>Example Mart</Text>
-        </View>
-        <View style={styles.row}>
-          <Text style={styles.label}>Total:</Text>
-          <Text style={[styles.content, styles.total]}>$50.00</Text>
+        <View style={styles.dataContainer}>
+          <DataItem firstEntry="Amount" secondEntry="$1500" />
+          <DataItem
+            firstEntry="Transfer method"
+            secondEntry="Transfer Wallet"
+          />
+          <DataItem firstEntry="Time" secondEntry="12:30 21/08/23" />
+          <DataItem firstEntry="Transaction ID" secondEntry="56472" />
         </View>
       </ViewShot>
-      <Button title="Take Screenshot" onPress={handleTakeScreenshot} />
+      <TouchableOpacity style={styles.button} onPress={handleTakeScreenshot}>
+        <Text style={styles.buttonText}>Take Screenshot</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // Container style with flex 1 to fill the screen
+    flex: 1,
+  },
+  headerContainer: {
+    backgroundColor: '#3363d5',
+    flex: 0.4,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  headerTitle: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 8,
+    marginBottom: 28,
+  },
+  transferToText: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  transferToContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 8,
+    borderWidth: 0.5,
+    borderRadius: 4,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    marginHorizontal: 20,
+    borderColor: '#91959f',
+    backgroundColor: '#fff',
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.2,
+        shadowRadius: 2,
+      },
+      android: {
+        elevation: 4,
+      },
+    }),
+  },
+  nameAndPhoneContainer: {
+    marginLeft: 12,
+    justifyContent: 'center',
+  },
+  nameText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  phoneText: {
+    fontSize: 12,
+  },
+  userDataContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 75,
+    borderWidth: 0.1,
+    borderColor: '#91959f',
   },
   receiptContainer: {
     flex: 1,
-    backgroundColor: '#f0f0f0', // Background color
-    padding: 20,
-    borderRadius: 10,
-    margin: 20,
-    elevation: 5,
+    backgroundColor: '#fff',
   },
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20, // Increased margin bottom for separation
-    textAlign: 'center', // Center text alignment
-    color: '#333', // Header text color
+  dataContainer: {
+    marginTop: 10,
+    borderColor: '#91959f',
+    borderWidth: 0.5,
+    borderRadius: 4,
+    marginHorizontal: 20,
+    backgroundColor: '#fff',
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.2,
+        shadowRadius: 2,
+      },
+      android: {
+        elevation: 4,
+      },
+    }),
   },
-  row: {
+  dataItemStyle: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginBottom: 15, // Increased margin bottom for separation
+    paddingHorizontal: 20,
+    paddingVertical: 10,
   },
-  label: {
+  firstEntryText: {
+    color: '#91959f',
+  },
+  secondEntryText: {
+    color: '#000',
+  },
+  button: {
+    backgroundColor: '#3363d5',
+    borderRadius: 50,
+    paddingVertical: 8,
+    paddingHorizontal: 28,
+    marginBottom: 20,
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
     fontWeight: 'bold',
-    color: '#555', // Label text color
-  },
-  content: {
-    color: '#222', // Content text color
-  },
-  total: {
-    color: 'green',
-    fontWeight: 'bold', // Bold text for total
   },
 });
 
